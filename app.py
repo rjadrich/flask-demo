69d6649835a424b487af7962f5f2bd0ce8427481
rjadrich <rjadrich@gmail.com>
2016-05-24 00:00:37 -0500

    final

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template, request, redirect
+from bokeh.plotting import figure
+from bokeh.embed import components 
+from bokeh.palettes import Spectral6
+import requests
+import pandas as pd
+import datetime
 
 app = Flask(__name__)
 
@@ -16,11 +22,29 @@ def index():
     app.vars['ticker'] = request.form['ticker']
     app.vars['ticker_options'] = request.form.getlist('ticker_options')
 
-    message = 'id chosen: %s options selected:' % app.vars['ticker']
-    for entry in app.vars['ticker_options']:
-        message = message + " " + entry
+    #get the data
+    api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json' % app.vars['ticker']
+    session = requests.Session()
+    session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
+    raw_data = session.get(api_url)
+    aapl_stock = raw_data.json()
 
-    return message
+    #make a datetime dataset
+    stock_df = pd.DataFrame(data = aapl_stock["data"], columns = aapl_stock["column_names"])[['Date','Open','Adj. Open','Close','Adj. Close']]
+    convert_to_dt = lambda x: datetime.datetime.strptime(x, "%Y-%m-%d")
+    stock_df['Date_dt'] = stock_df['Date'].apply(convert_to_dt)
+
+    #make the plot
+    TOOLS = 'box_zoom,box_select,crosshair,resize,reset'
+    plot = figure(tools=TOOLS, title='Data from Quandle WIKI set', x_axis_label='date', x_axis_type='datetime')
+    iteration = 0
+    for entry in app.vars['ticker_options']:
+        plot.line(stock_df['Date_dt'], stock_df[entry], size=20, color=Spectral6[iteration], legend=entry)
+        iteration = iteration + 1
+    script, div = components(plot)
+    header_text = "Stock data for %s" % app.vars['ticker']
+    return render_template('graph_ticker.html', script=script, div=div, header_text=header_text)
+
 
 if __name__ == '__main__':
   app.run(port=33507)
