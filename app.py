1dd54cfef72075725350c42692dbbf02e2469037
rjadrich <rjadrich@gmail.com>
2016-05-29 14:54:57 -0500

    improved interface

--- app.py
+++ app.py
@@ -21,11 +21,21 @@ def main():
 
 @app.route('/ticker_input', methods=['GET','POST'])
 def index():
+    github = 'https://github.com/rjadrich/flask-demo'
+    
     if request.method == 'GET':
-        return render_template('ticker_input.html', message="Please enter a stock ticker eg. GOOG, and check desired features.")
+        return render_template('ticker_input.html', message="Please enter a stock ticker (e.g. GOOG).", github=github)
     else:
         app.vars['ticker'] = request.form['ticker']
         app.vars['ticker_options'] = request.form.getlist('ticker_options')
+        
+    #error check 
+    if  app.vars['ticker'] == '':
+        return render_template('ticker_input.html', message="Nothing entered.", github=github)
+    
+    #error check 
+    if  len(app.vars['ticker_options']) == 0:
+        return render_template('ticker_input.html', message="No choices selected for %s." % app.vars['ticker'].upper(), github=github)
 
     #get the data
     api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json' % app.vars['ticker']
@@ -34,11 +44,9 @@ def index():
     raw_data = session.get(api_url)
     aapl_stock = raw_data.json()
     
-    #error check
+    #error check again
     if "error" in aapl_stock.keys():
-        return render_template('ticker_input.html', message="Stock %s does not exist. Try again." % app.vars['ticker'].upper())
-    if  len(app.vars['ticker_options']) == 0:
-        return render_template('ticker_input.html', message="No choices selected for %s. Try again." % app.vars['ticker'].upper())
+        return render_template('ticker_input.html', message="Stock %s does not exist." % app.vars['ticker'].upper(), github=github)
 
     #make a datetime dataset
     stock_df = pd.DataFrame(data = aapl_stock["data"], columns = aapl_stock["column_names"])[['Date','Open','Adj. Open','Close','Adj. Close']]
@@ -47,7 +55,7 @@ def index():
 
     #make the plot
     TOOLS = 'box_zoom,box_select,crosshair,resize,reset'
-    plot = figure(tools=TOOLS, title='Data from Quandle WIKI set', x_axis_label='date', x_axis_type='datetime')
+    plot = figure(tools=TOOLS, title='Data from Quandle WIKI set', x_axis_label='date', x_axis_type='datetime', width=700, height=400)
     iteration = 0
     for entry in app.vars['ticker_options']:
         plot.line(stock_df['Date_dt'], stock_df[entry], line_width=3, color=Spectral6[iteration], legend=entry)
@@ -55,7 +63,8 @@ def index():
     script, div = components(plot)
     header_text = "Stock data for %s" % app.vars['ticker'].upper()
     
-    return render_template('graph_ticker.html', script=script, div=div, header_text=header_text)
+    #return render_template('graph_ticker.html', script=script, div=div, header_text=header_text)
+    return render_template('ticker_input.html', script=script, div=div, message="Plotted stock %s below." % app.vars['ticker'].upper(), github=github)
 
 if __name__ == '__main__':
     app.debug = True
